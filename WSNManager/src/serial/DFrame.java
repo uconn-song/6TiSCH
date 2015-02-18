889bb495baf35d5322408448155da739c55378d5
Marcus <Marcusli029@gmail.com>
2015-02-18 18:08:47 -0500

    update tostring for data

--- WSNManager/src/serial/DFrame.java
+++ WSNManager/src/serial/DFrame.java
@@ -1,11 +1,14 @@
 package serial;
 
+import jNetPcap.JPacketTest;
+
 import java.util.ArrayList;
 
 public class DFrame extends Frame {
 
 		//This frame is used by the mote to indicate to the computer it is ready to receive data.
 		private String _address="";
+		private byte[] ipv6data;
 		public DFrame(ArrayList<Byte> data) {
 			setType("Data");
 			setData(data);
@@ -16,13 +19,16 @@ public class DFrame extends Frame {
 			data.get(5);//asn 2
 			data.get(6);//asn 3
 			data.get(7);//asn 4
+			ipv6data = new byte[data.size()-10];
 			for(int i = 8 ; i < data.size()-2;i++)
 			{
+				ipv6data[i-8]=data.get(i);
 				//System.out.print("Data: " + byteToString(data.get(i))+"(0x"+Integer.toHexString(data.get(i)&0xFF)+") ");
 			}
 			//System.out.println();
 			//HDLC.crcValidate(data);
-			System.out.println();
+			
+			new JPacketTest(ipv6data);
 
 		}
 		
@@ -40,6 +46,12 @@ public class DFrame extends Frame {
 				for(int i = 8 ; i < _data.size()-2;i++)
 				{
 					toret = toret+" " + byteToString(_data.get(i));
+				}
+				
+				toret = toret+"\n";
+				for(int i = 8 ; i < _data.size()-2;i++)
+				{
+					toret = toret+" " + Integer.toHexString(_data.get(i));
 				}
 			}
 			return toret;
