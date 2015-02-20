4e386b4ac6b4a50b3f472b0f7eec7240c19ce455
Marcus <Marcusli029@gmail.com>
2015-02-19 20:48:40 -0500

    extension of JPanel

---
+++ WSNManager/src/gui_components/ContentPanel.java
@@ -0,0 +1,33 @@
+package gui_components;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+/**
+ * Almost the same thing as a JPanel just a "frame" for a content area on the
+ * program
+ * 
+ * @author Marcus
+ */
+public class ContentPanel extends JPanel {
+	public ContentPanel() {
+		setLayout(new GridBagLayout());
+		setVisible(true);
+	}
+
+	public void switchComponent(JComponent p) {
+		//clear the panel
+		try {
+			this.remove(0);
+		} catch (ArrayIndexOutOfBoundsException e) {}
+		//add a new component which will fill the content panel
+		GridBagConstraints fillArea = new GridBagConstraints();
+		fillArea.fill = GridBagConstraints.BOTH;
+		fillArea.weightx = 1.0;
+		fillArea.weighty = 1.0;
+		add(p, fillArea);
+	}
+}
